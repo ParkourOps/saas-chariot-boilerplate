b6e49c41253f4686bb9b5810b0d94e16bf006ca4
Tej Birring <tejbirring@gmail.com>
2024-03-17 15:00:29 +0000

    Remove redundant (Vue template) components. Implement top bar and clean up app layout.

---
+++ spa/src/components/TheTopBar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header class="bg-primary-500 shadow">
+        <div class="flex flex-row justify-center sm:justify-between w-full max-w-screen-xl mx-auto text-surface-0 p-4">
+            <h1 class="font-extrabold text-xl sm:text-3xl w-fit">
+                SaaS Chariot
+            </h1>
+        </div>
+    </header>
+</template>
