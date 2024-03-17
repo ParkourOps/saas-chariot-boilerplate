ad615e4e09b24b191de4b8f807839acd12bc2930
Tej Birring <tejbirring@gmail.com>
2024-03-17 21:01:16 +0000

    Implement busy state store.

---
+++ spa/src/framework/libraries/busy-status.ts
@@ -0,0 +1,36 @@
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+import UniqueIdGenerator from "@/common/libraries/unique-id-generator"
+
+type PendingAction = {
+    id: string
+}
+
+const uniqueIdGenerator = new UniqueIdGenerator();
+
+const useBusyStatus = defineStore("Busy", ()=>{
+    const pendingActions = ref<Array<PendingAction>>([]);
+
+    function registerPendingAction() {
+        const id = uniqueIdGenerator.generate();
+        console.debug(`Doing: ${id}`);
+        pendingActions.value.push({
+            id,
+        });
+        return {
+            unregisterPendingAction() {
+                console.debug(`Done: ${id}`);
+                pendingActions.value = pendingActions.value.filter((a)=>a.id !== id);
+            },
+        };
+    }
+
+    const busy = computed(() => pendingActions.value.length > 0);
+
+    return {
+        registerPendingAction,
+        busy
+    }
+});
+
+export default useBusyStatus;
