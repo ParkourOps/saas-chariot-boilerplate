eeccf1c4763ee60f5dfc52fc30bd40b15e3e5e5a
Tej Birring <tejbirring@gmail.com>
2024-03-17 17:16:39 +0000

    Implement SingleCardLayout.

---
+++ spa/src/components/layouts/SingleCardLayout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <section>
+        <Card class="max-w-md mx-auto sm:mt-12 overflow-hidden">
+            <template #header>
+                <slot name="header" />
+            </template>
+            <template #content>
+                <slot />
+            </template>
+            <template #footer>
+                <slot name="footer" />
+            </template>
+        </Card>
+    </section>
+</template>
