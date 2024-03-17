6e19fbe18e33fb6709e5989fb3356bb71a6ebce8
Tej Birring <tejbirring@gmail.com>
2024-03-17 17:17:08 +0000

    Implement schema-validated textbox input.

---
+++ spa/src/components/controls/inputs/FormInputText.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import Validator from '@/common/libraries/validator';
+import { computed, watch } from 'vue';
+import type { ZodString } from 'zod';
+
+    const props = defineProps<{
+        name: string,
+
+        label: string,
+        icon?: string
+        help?: string,
+        required?: boolean
+        
+
+        align: "left" | "right" | "center",
+        size: "small" | "normal" | "large",
+
+        schema: ZodString
+    }>();
+
+    const alignmentClassesLabel = computed(()=>{
+        switch (props.align) {
+            case 'left':
+                return "self-start";
+            case 'right':
+                return "self-end";
+            case 'center':
+                return "self-center";
+        }
+    });
+
+    const alignmentClassesInput = computed(()=>{
+        switch (props.align) {
+            case 'left':
+                return "text-left";
+            case 'right':
+                return "text-right";
+            case 'center':
+                return "text-center";
+        }
+    });
+
+    const model = defineModel<string>();
+
+    const validator = new Validator(props.schema);
+    const valid = defineModel<boolean>("valid");
+    watch(model, (val)=>{
+        valid.value = validator.isValid(val);
+    });
+</script>
+
+<template>
+    <div class="flex flex-col gap-2">
+        <label :for="name" class="flex flex-row items-center" :class="[alignmentClassesLabel]">
+            <Vector :name="icon" class="size-4 mr-1" v-if="icon && (props.align === 'left' || props.align === 'center')" />
+            {{ label }}
+            <Vector :name="icon" class="size-4 ml-1" v-if="icon && (props.align === 'right')" />
+        </label>
+        <InputText :id="name" :aria-describedby="help ? `${name}-help` : undefined" :size="(size !== 'normal') ? size : undefined" :class="[alignmentClassesInput]" v-model="model" :invalid="!!model && !valid" />
+        <small :id="`${name}-help`" v-if="help" :class="[alignmentClassesLabel]">{{ help }}</small>
+    </div>
+</template>
