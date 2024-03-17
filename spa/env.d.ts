96edf6907e7e88940cc213efa9436eb77b2d5e1c
Tej Birring <tejbirring@gmail.com>
2024-03-17 01:34:20 +0000

    Add env variable to define default value of HTML->Head->Title (until overriden by JS code in runtime).

--- spa/env.d.ts
+++ spa/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    // Site Title
+    readonly VITE_DEFAULT_TITLE: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
