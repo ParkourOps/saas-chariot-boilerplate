194f9525aa2a42789ae59702e4debfb601ca558e
Tej Birring <tejbirring@gmail.com>
2024-03-17 01:53:52 +0000

    Integrate Firebase client-side library.

---
+++ spa/src/framework/libraries/firebase/functions.ts
@@ -0,0 +1,14 @@
+import app from "./app";
+import {getFunctions, connectFunctionsEmulator} from "firebase/functions";
+
+const functions = getFunctions(app);
+
+(() => {
+    const useEmulator = import.meta.env.VITE_FIREBASE_USE_EMULATOR.toLowerCase();
+    if (/(true|yes|t|y)/i.test(useEmulator)) {
+        connectFunctionsEmulator(functions, "127.0.0.1", 5001);
+        console.debug("Connected to local Firebase emulator.");
+    }
+})();
+
+export default functions;
