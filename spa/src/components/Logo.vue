361cad0bdf7f952d8b231307389750be7b0b7929
Tej Birring <tejbirring@gmail.com>
2024-03-17 17:16:06 +0000

    Add dummy Logo component.

---
+++ spa/src/components/Logo.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+    <!-- The SaaS Chariot Logo is textual, but you can replace this with whatever you want. -->
+    <h1 class="font-extrabold text-xl sm:text-3xl w-fit">
+        SaaS Chariot
+    </h1>
+</template>
