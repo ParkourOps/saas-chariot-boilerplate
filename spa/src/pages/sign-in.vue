70dfd67589d09ec512b6d1a49893375a54700d3b
Tej Birring <tejbirring@gmail.com>
2024-03-19 17:11:26 +0000

    Clean up sign in page.

--- spa/src/pages/sign-in.vue
+++ spa/src/pages/sign-in.vue
@@ -4,6 +4,7 @@ import {useUserAuthentication} from '@/framework/features/user-authentication';
 import actor from '@/framework/libraries/actor';
 import Actor from '@/framework/libraries/actor';
 import logger from '@/framework/libraries/logger';
+import handleErrorInUI from '@/framework/utilities/handle-error-in-ui';
 import { useToast } from 'primevue/usetoast';
 import { onMounted, ref } from 'vue';
 const userAuthentication = useUserAuthentication();
@@ -26,25 +27,20 @@ const toast = useToast();
                 {name: '/sign-in'},
             );
             toast.add({
-                severity: "success",
+                severity: "info",
                 summary: "Link sent!",
                 detail: "Please check your inbox."
             });
         } catch (e) {
-            if (e instanceof Error) {
-                logger.error(e);
-                toast.add({
-                    severity: "error",
-                    summary: "Link not sent.",
-                    detail: e.message,
-                });
-            } else {
-                toast.add({
-                    severity: "error",
-                    summary: "Link not sent.",
-                    detail: "Something went wrong. Please try again and get in touch if this error persists."
-                });
-            }
+            handleErrorInUI(toast, e, "Failed to send link.", "Something went wrong while trying to send your sign in link (probably on our side). Please try again and get in touch if this error persists.");
+        }
+    });
+
+    const signInWithThirdPartyAccount = (provider: Parameters<typeof userAuthentication.signInWithThirdPartyAccount>[0]) => Actor.act(async ()=>{
+        try {
+            await userAuthentication.signInWithThirdPartyAccount(provider);
+        } catch (e) {
+            handleErrorInUI(toast, e, "Failed to sign in.", "Something went wrong while trying to sign in with your third-party account account. Please try again and get in touch if this error persists.");
         }
     });
 
@@ -55,14 +51,7 @@ const toast = useToast();
                 try {
                     await userAuthentication.signInWithLink.catchSignInWithLinkAttempt();
                 } catch (e) {
-                    if (e instanceof Error) {
-                        logger.error(e);
-                        toast.add({
-                            severity: "error",
-                            summary: "Could not sign in.",
-                            detail: e.message,
-                        });
-                    }
+                    handleErrorInUI(toast, e, "Failed to sign in.", "Something went wrong while trying to sign in with link. Please try again and get in touch if this error persists.");
                 }
             },
             // use email stored from previous attempt if available
@@ -76,20 +65,34 @@ const toast = useToast();
 <template>
     <SingleCardLayout>
         <template #header>
-            <div class="pt-6 sm:pt-8 flex flex-col gap-4">
-                <Logo class="mx-auto" />
-                <p class="text-center max-w-80 mx-auto">Please enter your email address and we'll send you a sign in link.</p>
-            </div>
         </template>
 
         <template #default>
             <div class="flex flex-col gap-4">
+                <Logo class="mx-auto" />
+                <p class="text-center max-w-96 mx-auto px-4 my-4">Please enter your email address and we'll send you a sign in link.</p>
                 <FormInputText name="email" label="Email" align="center" icon="fi-ss-envelope" v-model="emailAddress" v-model:valid="emailAddressValid" :schema="EmailAddress" required size="large" />
             </div>
         </template>
 
         <template #footer>
-            <Button class="w-full" :disabled="!emailAddressValid" @click="sendSignInLink">Send</Button>
+            <Button class="w-full" :disabled="!emailAddressValid" @click="sendSignInLink">Get Link</Button>
+            <Divider />
+            <p class="text-center my-8">Or sign in with...</p>
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                <Button class="w-full" severity="secondary" @click="signInWithThirdPartyAccount('google')()">
+                    <Vector name="google" class="size-5 fill-white dark:fill-surface-900" />
+                </Button>
+                <Button class="w-full" severity="secondary" @click="signInWithThirdPartyAccount('github')()">
+                    <Vector name="github" class="size-6 fill-white dark:fill-surface-900" />
+                </Button>
+                <Button class="w-full" severity="secondary" @click="signInWithThirdPartyAccount('twitter')()">
+                    <Vector name="twitter" class="size-6 fill-white dark:fill-surface-900" />
+                </Button>
+                <Button class="w-full" severity="secondary" @click="signInWithThirdPartyAccount('facebook')()">
+                    <Vector name="facebook" class="size-6 fill-white dark:fill-surface-900" />
+                </Button>
+            </div>
         </template>
     </SingleCardLayout>
 </template>
