1b7dabffdac2ba05990fc4ddf371ed4cdecfacc5
Tej Birring <tejbirring@gmail.com>
2024-03-19 03:29:56 +0000

    Implement basic types for logging.

---
+++ spa/src/framework/types/index.ts
@@ -0,0 +1,6 @@
+import type { Loggable } from "@/_common_/types/loggable";
+
+type ApplicationLoggable<Type extends string, TypeData extends object> = Loggable<`${Type}@spa`, TypeData>;
+
+export type ApplicationError = ApplicationLoggable<"error", {}>;
+export type ApplicationDebug = ApplicationLoggable<"debug", {}>;
