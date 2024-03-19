afac263d39655461c7bbbc60b00eac600d479819
Tej Birring <tejbirring@gmail.com>
2024-03-19 03:16:08 +0000

    Correct typo.

--- functions/src/features/email-messaging/api/send-text-only-email.ts
+++ functions/src/features/email-messaging/api/send-text-only-email.ts
@@ -1,8 +1,8 @@
-import SendTemplatedEmail from "@/_common_/features/email-messaging/api/SendTextOnlyEmail";
+import SendTextOnlyEmail from "@/_common_/features/email-messaging/api/SendTextOnlyEmail";
 import {sendEmail} from "@/services/sendgrid";
 import apiEndpointFactory from "@/libraries/api-endpoint-factory";
 
-export default apiEndpointFactory.createAPIEndpoint(SendTemplatedEmail)(async (request)=>{
+export default apiEndpointFactory.createAPIEndpoint(SendTextOnlyEmail)(async (request)=>{
     await sendEmail(request.correlationId, request.data);
     return {};
 });
