214d8baf14f82390d80d394bc66b4ac038348bc1
Tej Birring <tejbirring@gmail.com>
2024-03-19 03:11:30 +0000

    Abbrieviate loggable types.

--- functions/src/types/index.ts
+++ functions/src/types/index.ts
@@ -1,25 +1,19 @@
+import { Loggable } from "@/_common_/types/loggable";
 import { FunctionsErrorCode } from "firebase-functions/v2/https";
 
 export type CorrelationID<Prefix extends string> = `${Prefix}:${string}`;
 
-type FunctionLoggable<Type extends string, Prefix extends string, TypeData extends object | null> = Loggable<`${Type}:fncs:${Prefix}`, TypeData> & {
-    correlationID: CorrelationID<Prefix>
-}
+type FunctionLoggable<Type extends string, Prefix extends string, TypeData extends object> = Loggable<
+    `${Type}@fncs:${Prefix}`,
+    TypeData & {
+        correlationID: CorrelationID<Prefix>
+    }
+>;
 
 export type ErrorCode = FunctionsErrorCode;
-export type ErrorMessage = string;
-export type ErrorDetails = Record<string | number, unknown>;
 
 export type FunctionError<Prefix extends string> = FunctionLoggable<"error", Prefix, {
     code: ErrorCode,
-    message: ErrorMessage,
-    details: ErrorDetails,
 }>;
 
-export type DebugMessage = string;
-export type DebugDetails = Record<string | number, unknown>;
-
-export type FunctionDebug<Prefix extends string> = FunctionLoggable<"debug", Prefix, {
-    message: DebugMessage,
-    details: DebugDetails
-}>;
+export type FunctionDebug<Prefix extends string> = FunctionLoggable<"debug", Prefix, {}>;
