fa851ca6b4a370227fb7c169c2018af070399ea7
Tej Birring <tejbirring@gmail.com>
2024-03-19 17:55:50 +0000

    Remove log method from ApplicationError. Use the logger for any logging.

--- spa/src/framework/libraries/application-error.ts
+++ spa/src/framework/libraries/application-error.ts
@@ -1,5 +1,4 @@
 import type { LogDetails, LogMessage } from "@/_common_/types/loggable";
-import logger from "./logger";
 
 export class ApplicationError extends Error {
     readonly message;
@@ -8,8 +7,5 @@ export class ApplicationError extends Error {
         super(message);
         this.message = message;
         this.details = details;
-    }
-    log() {
-        logger.error(this);
     }
 }
