88adffada8423cb93919976c6196a994b01c5332
Tej Birring <tejbirring@gmail.com>
2024-03-20 01:59:10 +0000

    Minor refactor.

--- spa/src/framework/utilities/get-route-url.ts
+++ spa/src/framework/utilities/get-route-url.ts
@@ -1,7 +1,7 @@
 import router from "@/router";
 import type { RouteLocationRaw } from "vue-router/auto";
 
-export default function (route: RouteLocationRaw) {
+export default function(route: RouteLocationRaw) {
     const _route = router.resolve(route);
     return window.location.origin + _route.href;
 }
--- spa/src/framework/utilities/handle-error-in-ui.ts
+++ spa/src/framework/utilities/handle-error-in-ui.ts
@@ -2,7 +2,7 @@ import type { ToastServiceMethods } from "primevue/toastservice";
 import { ApplicationError } from "../libraries/application-error";
 import logger from "../libraries/logger";
 
-export default function handleErrorInUI(toast: ToastServiceMethods, error: unknown, genericErrorSummary: string, genericErrorMessage: string) {
+export default function(toast: ToastServiceMethods, error: unknown, genericErrorSummary: string, genericErrorMessage: string) {
     logger.error(error, undefined, {
         genericErrorSummary,
         genericErrorMessage
